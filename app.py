9dead4e9ba6e3534e42ecdd210b7007b9069619b
João Gabriel Grandotto Viana <63874169+joaograndotto@users.noreply.github.com>
2022-10-26 15:04:36 -0300

    Codigo atualizado

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import pandas as pd
 import pickle
 
 
-
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,7 +13,7 @@ def hello():
 #carregar modelo
 def classify_utterance(utt):
     # load the model
-    loaded_model = pickle.load(open('models/fiocruz.novaia.model', 'rb'))
+    loaded_model = pickle.load(open('models\ifgIASigmond.model', 'rb'))
     # make a prediction
     resultado = loaded_model.predict(utt)
     return resultado
@@ -26,21 +24,23 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 #request pelo arquivo csv 
 @app.route("/request_csv", methods=['POST'])
 def uploadFiles():
-      #upload do arquivo
-      uploaded_file = request.files['file']
-      if uploaded_file.filename != '':
-           #caminho para o upload do arquivo
-           file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-           #salvando arquivo
-           uploaded_file.save(file_path)
-           #lendo csv não classificado
-      dataset = pd.read_csv(file_path, sep=',', encoding='utf-8')
-      predict = classify_utterance(dataset['Affiliations'])
-      # Transformando a lista(numpy) em uma dataframe
-      dataset_classificado = pd.DataFrame (predict, columns = ['Affiliations'])
-      #Salvando o arquivo classificado localmente
-      dataset_classificado.to_csv("static/Classificado.csv", index=False)
-      return ''' <h3><a href="static/Classificado.csv" download>Clique Aqui para fazer o download</a></h3>''' #return para o download do arquivo classificado
+    #upload do arquivo
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        #caminho para o upload do arquivo
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
+        #salvando arquivo
+        uploaded_file.save(file_path)
+
+    #lendo csv não classificado
+    dataset = pd.read_csv(file_path, sep=',', encoding='utf-8')
+    predict = classify_utterance(dataset.squeeze())
+    
+    dataset["afiliacao_classificada"] = predict
+    dataset['afiliacao_classificada'] = dataset['afiliacao_classificada'].apply(lambda x: "Instituto Federal de Goiás" if x == 1 else 'Não é Instituto Federal de Goiás')
+    #Salvando o arquivo classificado localmente
+    dataset.to_csv("static/arquivo_classificado.csv", index=False)
+    return ''' <h3><a href="static/arquivo_classificado.csv" download>Clique Aqui para fazer o download</a></h3>''' #return para o download do arquivo classificado
 
 
 #request pelo html
@@ -49,7 +49,8 @@ def request_input():
     affiliation = request.form.get('affiliation')
     affiliation = [affiliation]
     result = classify_utterance(affiliation)
+    r = ['Não é Instituto Federal de Goiás','É Instituto Federal de Goiás']
     return '''
-                  <h1>A afiliação é: {}</h1>'''.format(result[0])
+                  <h1>A afiliação é: {}</h1>'''.format(r[result[0]])
 if __name__ == '__main__':
     app.run(debug=True)
